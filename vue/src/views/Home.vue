c2e30f45ea4f6ca40dc9d0f7dbd427423affcb74
gbogh <gboghmf@gmail.com>
2021-07-19 17:56:17 +0900

    commit

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Don't try this at home</h1>
+    <p>{{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name : 'WWE Superstar John Cina'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  font-size:20px;
+  color : red;
+}
+</style>
